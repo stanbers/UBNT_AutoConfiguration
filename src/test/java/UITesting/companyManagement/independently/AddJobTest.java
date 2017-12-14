cece0a2a18212decaab7966b4300c82aa88d3d5c
stanbers <stanxu526@gmail.com>
2017-12-14 14:20:51 +0800

    add 'addStuffTest' and 'addJobTest' automation script

---
+++ src/test/java/UITesting/companyManagement/independently/AddJobTest.java
@@ -0,0 +1,9 @@
+package UITesting.companyManagement.independently;
+
+/**
+ * @Author by XuLiang
+ * @Date 2017/12/14 9:38
+ * @Email stanxu526@gmail.com
+ */
+public class AddJobTest {
+}
---
+++ src/test/java/UITesting/companyManagement/independently/AddStuffTest.java
@@ -0,0 +1,9 @@
+package UITesting.companyManagement.independently;
+
+/**
+ * @Author by XuLiang
+ * @Date 2017/12/14 10:30
+ * @Email stanxu526@gmail.com
+ */
+public class AddStuffTest {
+}
