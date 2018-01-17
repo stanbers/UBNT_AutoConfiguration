82e7872edf4aff36a4ed150e1e227739b79233b9
stanbers <stanxu526@gmail.com>
2018-01-17 19:01:11 +0800

    test local

---
+++ src/main/java/gui/version/wallhanging/WallHangingMain.java
@@ -0,0 +1,12 @@
+package gui.version.wallhanging;
+
+/**
+ * @Author by XuLiang
+ * @Date 2018/01/17 9:58
+ * @Email stanxu526@gmail.com
+ */
+public class WallHangingMain {
+    public static void main(String[] args) {
+        new WallHanging().config("test","192.168.0.101","255.255.255.0","192.168.0.1","192.168.0.98");
+    }
+}
